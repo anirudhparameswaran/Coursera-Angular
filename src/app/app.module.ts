1d45935b3dc455d9c27d63989f9bc64281139cdb
Anirudh parameswaran <anirudhparameswaran@gmail.com>
2019-06-28 21:51:51 +0530

    Single Page Application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 @NgModule({
@@ -47,7 +48,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     AppRoutingModule   
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shared/dishes.ts
+++ src/app/shared/dishes.ts
@@ -4,7 +4,7 @@ export const DISHES: Dish[] = [
     {
         id: '0',
         name: 'Uthappizza',
-        image: '/assets/images/uthappizza.png',
+        image: '/assets/images/download.jpg',
         category: 'mains',
         featured: true,
         label: 'Hot',
